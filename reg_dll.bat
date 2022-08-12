d9d97919061d2ea6e447cfcf0c85a276632617cd
bormen001 <111103487+bormen001@users.noreply.github.com>
2022-08-12 08:23:02 +0300

    Add files via upload

---
+++ reg_dll.bat
@@ -0,0 +1,5 @@
+
+set PATH=c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe C:\Users\Alex_Nout\source\repos\CL_1C\bin\Debug\CL_1C.dll  /codebase
+
+pause
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe C:\Users\Alex_Nout\source\repos\CL_1C\bin\Debug\CL_1C.dll /codebase
